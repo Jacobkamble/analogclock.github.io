f33d4662a7d30964880d1d534cd6b5946c08a10c
Jacobkamble <76991085+Jacobkamble@users.noreply.github.com>
2022-08-02 11:38:24 +0530

    first

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("testi");
+
+//      hours = document.getElementById("hours");
+//  minute = document.getElementById("minute");
+//  second = document.getElementById("second");
+ 
+
+
+setInterval(() => {
+
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    hrotation = 30 * htime + mtime / 2;
+    mrotation = 6 * mtime;
+    srotation = 6 * stime;
+
+    hours.style.transform = `rotate(${hrotation}deg)`;
+
+    minute.style.transform = `rotate(${mrotation}deg)`;
+
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000)
+
+
+
+
+
+
